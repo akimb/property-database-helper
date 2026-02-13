22ec6621334022dfd76131b8967cdfc8aee41855
Andrew Kim <ndbkim@gmail.com>
2026-02-13 00:54:41 -0800

    add new_property and create_property for adding new properties. also added comment blocks for methods

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,10 +3,15 @@ class ImportsController < ApplicationController
   end
 
   def create
+    # ---------------------------------------------------------------------------------
+    # Creates a new database based on importing the raw CSV.
+    # Then calls the CsvImportService controller and creates an
+    # ImportProperty for each row. 
+    # ---------------------------------------------------------------------------------
     uploaded_file = params[:file]
     import = Import.create!(
       filename: uploaded_file.original_filename,
-      raw_csv: uploaded_file.read,
+      raw_csv: uploaded_file.read, # keep original csv if desired
       status: "pending"
     )
     CsvImportService.new(import.raw_csv, import).call
@@ -15,24 +20,50 @@ class ImportsController < ApplicationController
   end
 
   def show
+    # ---------------------------------------------------------------------------------
+    # Method to list out all properties in the database.
+    # ---------------------------------------------------------------------------------
     @import = Import.find(params[:id])
   end
 
-  def confirm
+  # def confirm
 
+  # end
+
+  def new_property
+    @import = Import.find(params[:id])
+    @property = @import.imported_properties.new
   end
 
+  def create_property
+    @import = Import.find(params[:id])
+    @property = @import.imported_properties.new(imported_property_params)
+    @property.zip_valid = CsvImportService.valid_zip?(params[:zip_code], params[:state])
+    
+    if @property.save
+      redirect_to import_path(@import)
+    else
+      render :new_property
+    end
+  end
+  
   def edit
+    # ---------------------------------------------------------------------------------
+    # Method to access the edit form by the property name as its unique identifier
+    # ---------------------------------------------------------------------------------
     @import = Import.find(params[:id])
     @property = @import.imported_properties.find_by!(name: params[:property_name])
   end
 
   def update
+    # ---------------------------------------------------------------------------------
+    # method: :patch method recognized by Rails.
+    # The specific property is found, then uses the update! method via Rails
+    # to persist changes to the database.
+    # ---------------------------------------------------------------------------------
     @import = Import.find(params[:id])
     @property = @import.imported_properties.find_by!(name: params[:property_name])
 
-    # REFACTORED: Use Rails update! to persist changes to database
-    # This is much simpler and safer than CSV string reconstruction
     @property.update!(
       name: params[:name],
       street_address: params[:street_address],
@@ -46,11 +77,22 @@ class ImportsController < ApplicationController
   end
 
   def destroy
+    # ---------------------------------------------------------------------------------
+    # Method to delete a property. Gets an instance of the property, 
+    # hence the lack of '@' on the property variable.
+    # ---------------------------------------------------------------------------------
     @import = Import.find(params[:id])
     import_id = @import.id
     property = @import.imported_properties.find_by!(name: params[:property_name])
     property.destroy
 
     redirect_to import_path(import_id)
+  end
+
+  private
+
+  def imported_property_params
+    # params.require(:imported_property).permit(:name, :street_address, :city, :state, :zip_code, units: [])
+    params.permit(:name, :street_address, :city, :state, :zip_code, units: [])
   end
 end
