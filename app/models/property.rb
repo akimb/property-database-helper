96222f1aadc064b04dadd953f5fb95c18a5b7769
Andrew Kim <ndbkim@gmail.com>
2026-02-12 17:04:33 -0800

    create property model

---
+++ app/models/property.rb
@@ -0,0 +1,7 @@
+class Property < ApplicationRecord
+  has_many :units # important to say for having multiple units
+
+  ## IMPORTANT FOR VALIDATING DUPLICATES
+  validates :name, presence: true, uniqueness: { case_sensitive: false } # validation for naming and uniqueness without case sensitivity
+
+end
