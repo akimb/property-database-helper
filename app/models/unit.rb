d9cdbaf6bf05dac663fdaafd6d3dea48bb4b0a75
Andrew Kim <ndbkim@gmail.com>
2026-02-17 00:47:59 -0800

    updated unit model

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,11 +1,15 @@
 class Unit < ApplicationRecord
   belongs_to :property, optional: true
 
-  belongs_to :imported_property
+  belongs_to :imported_property, optional: true
   # belongs_to :import, optional: true
 
   validates :unit_number, presence: true # makes it so units cannot be left blank if the record exists
 
   # prevents duplicate units
-  validates :unit_number, uniqueness: { scope: [:property_id, :imported_property_id], message: "Must be unique per property."} 
+  validates :unit_number, uniqueness: { 
+    scope: [:property_id, 
+    :imported_property_id], 
+    message: "must be unique per property."
+  } 
 end
